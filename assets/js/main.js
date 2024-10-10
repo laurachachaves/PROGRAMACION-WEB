893d8cae21ea28cd6caa6cac3c993186b223c7db
Your Name <laura.chaves@mail.ucv.es>
2024-10-10 14:36:52 +0200

    Clase dia 10

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@
 /* Ignoramos esta parte por el momento */
 /*****************************************/
 let intervalId = window.setInterval(function () {
-    console.log("Clock isn't working yet!");
+    //console.log("Clock isn't working yet!");
 }, 10000);
 
 function stopClock() { 
@@ -11,5 +11,25 @@ function stopClock() {
 /*****************************************/
 
 window.onload = function () { 
-    console.log("Hello, world!");
+    const miReloj = document.getElementById("clock");
+    console.log(miReloj);
+   console.dir(miReloj);
+   
+   
+   miReloj.innerText = "Hola Hola :)";
+
+   const headers = document.getElementsByTagName("div");
+   console.dir(headers);
+
+}
+
+function randint(min, max) {
+    return Math.floor(Math.random()*(max - min + 1))+ min;
+}
+
+function changeEmojiColor(elem) {
+    const r = randint(0, 255);
+    const g = randint(0, 255);
+    const b = randint(0, 255);
+    elem.style.fill = "rgb(" + r + "," + g + "," + b + ")";
 }
